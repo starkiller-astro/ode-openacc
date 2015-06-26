af2ba21c77847e887d4e97e474624b76ff1c60f0
Michael Zingale <michael.zingale@stonybrook.edu>
2015-06-26 15:02:23 -0400

    the driver

---
+++ first-order/test_react.f90
@@ -0,0 +1,33 @@
+program main
+
+  use network
+
+  implicit none
+
+  double precision :: Xin(nspec), Xout(nspec)
+  double precision :: rho, temp
+  double precision :: tmax
+  double precision enucdot
+
+  integer :: ic12, io16
+
+  ! initialize the network
+  call network_init()
+
+  ! setup the composition
+  ic12 = network_species_index("carbon-12")
+  io16 = network_species_index("oxygen-16")
+  Xin(:) = 0
+  Xin(ic12) = 0.5d0
+  Xin(io16) = 0.5d0
+
+  ! burn
+  temp = 8.e8
+  rho = 2.6e9
+  tmax = 1000.0
+
+  call react(Xin, temp, rho, tmax, Xout, enucdot)
+
+  print *, Xout
+
+end program main
