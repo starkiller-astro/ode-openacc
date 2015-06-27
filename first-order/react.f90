f577a20b8860d94e8cbb4199ae328c9428424b04
Michael Zingale <michael.zingale@stonybrook.edu>
2015-06-26 22:19:09 -0400

    fix the integration -- this now does a real Newton iteration to find a converged correction each timestep.

--- first-order/react.f90
+++ first-order/react.f90
@@ -24,8 +24,8 @@ subroutine react(Xin, T, rho, tmax, Xout, enucdot)
     
   double precision :: dXdt(nspec)
   double precision :: X1(nspec), X2(nspec), dX1dt(nspec), dX2dt(nspec)
+  double precision :: X_n(nspec), X_np1(nspec), dX(nspec), dfdX(nspec)
   double precision :: A(nspec, nspec), J(nspec, nspec), b(nspec)
-  double precision :: dfdX
   integer :: info
   
   double precision, parameter :: eps = 1.d-8
@@ -37,6 +37,11 @@ subroutine react(Xin, T, rho, tmax, Xout, enucdot)
 
   integer :: ic12, io16
 
+  double precision, parameter :: tol = 1.d-6
+  integer, parameter :: max_iter = 10
+  integer :: iter
+  logical :: converged
+  
   ic12 = network_species_index("carbon-12")
   io16 = network_species_index("oxygen-16")
 
@@ -50,68 +55,93 @@ subroutine react(Xin, T, rho, tmax, Xout, enucdot)
   print *, dXdt
 
   dt = 1.d33
-  dt = min(dt, abs(Xin(ic12)/dXdt(ic12)))
+  dt = 0.1*min(dt, abs(Xin(ic12)/dXdt(ic12)))
 
   print *, "dt = ", dt
 
-  Xout(:) = xin(:)
+
+  X_n(:) = Xin(:)
   
   ! do the integration
   time = 0.0d0
   do while (time < tmax)
 
-     ! construct a numerical Jacobian via simple differencing
-     ! Lapack matrix ordering is described here:
-     ! http://www.netlib.org/lapack/lug/node116.html#subsecarrayargs
-     ! in general, A(i,j) holds matrix element a_{i,j}
+     print *, time
 
-     ! J_{m,n} = df_m/dX_n
-     do n = 1, nspec
-        X1(:) = Xout(:)
-        X1(n) = X1(n) + eps
+     converged = .false.
 
-        X2(:) = Xout(:)
-        X2(n) = X2(n) - eps
-
-        call rhs(nspec, 0.0d0, X1, dX1dt, rpar, ipar)
-        call rhs(nspec, 0.0d0, X2, dX2dt, rpar, ipar)
-        
-        do m = 1, nspec
-           dfdX = 0.5d0*(dX2dt(m) - dX1dt(m))/(X2(n) - X1(n))
-           J(m,n) = dfdX
-        enddo
-     enddo
-
-     ! construct the matrix for the linear system
-     ! (I - dt A) X^{n+1} = X^n
-     do n = 1, nspec
-        do m = 1, nspec
-           if (m == n) then
-              I = 1.0d0
-           else
-              I = 0.0d0
-           endif
-           
-           A(m,n) = I - dt*J(m,n)
-        enddo
-
-        b(n) = xout(n)
-     enddo
+     ! initial guess
+     X_np1(:) = X_n(:)
      
-     ! solve the linear system -- we'll just use Lapack.  Note that the
-     ! input X is overwritten on output
-     call dgesv(nspec, 1, A, nspec, ipiv, b, nspec, info)
+     do iter = 1, max_iter
 
-     do n = 1, nspec
-        xout(n) = b(n)
-     enddo
-          
+        ! construct a numerical Jacobian via simple differencing
+        ! Lapack matrix ordering is described here:
+        ! http://www.netlib.org/lapack/lug/node116.html#subsecarrayargs
+        ! in general, A(i,j) holds matrix element a_{i,j}
+        
+        ! J_{m,n} = df_m/dX_n
+        do n = 1, nspec
+           X1(:) = X_np1(:)
+           X1(n) = X1(n) + eps
+           
+           X2(:) = X_np1(:)
+           X2(n) = X2(n) - eps
+           
+           call rhs(nspec, 0.0d0, X1, dX1dt, rpar, ipar)
+           call rhs(nspec, 0.0d0, X2, dX2dt, rpar, ipar)
+           
+           do m = 1, nspec
+              J(m,n) = 0.5d0*(dX2dt(m) - dX1dt(m))/(X2(n) - X1(n))
+           enddo
+        enddo
+
+        ! construct the matrix for the linear system
+        ! (I - dt J) dX^{n+1} = rhs
+        do n = 1, nspec
+           do m = 1, nspec
+              if (m == n) then
+                 I = 1.0d0
+              else
+                 I = 0.0d0
+              endif
+              
+              A(m,n) = I - dt*J(m,n)
+           enddo
+        enddo
+        
+        ! construct the RHS
+        call rhs(nspec, 0.0d0, X_np1, dXdt, rpar, ipar)
+        b(:) = X_n(:) - X_np1(:) + dt*dXdt(:)
+     
+        ! solve the linear system -- we'll just use Lapack.  Note that the
+        ! input b is overwritten on output
+        call dgesv(nspec, 1, A, nspec, ipiv, b, nspec, info)
+
+        dX(:) = b(:)
+
+        X_np1(:) = X_np1(:) + dX(:)
+        
+        ! compare to see if we converged
+        if (abs(dX(1)) < tol*X_n(1)) then
+           converged = .true.
+           exit
+        endif
+
+     enddo   ! dX iteration loop
+
+     if (.not. converged) then
+        stop 'convergence failure'
+     endif
+        
      if (time + dt > tmax) then
         dt = tmax - time
      endif
 
+     X_n(:) = X_np1(:)
      time = time + dt
   enddo
 
+  Xout(:) = X_n(:)
 
 end subroutine react
