1045423036f10080ec5e7e7c1fb5cd03af56f455
Michael Zingale <michael.zingale@stonybrook.edu>
2015-07-26 16:42:24 -0400

    add the openacc to the loop

--- first-order-rprox/react.f90
+++ first-order-rprox/react.f90
@@ -33,13 +33,13 @@ subroutine react(Xin, T, rho, tmax, Xout, ierr)
   real(kind=dp_t) :: time, dt, I
 
   integer :: m, n
-    
+
   real(kind=dp_t) :: dXdt(nspec)
   real(kind=dp_t) :: X1(nspec), X2(nspec), dX1dt(nspec), dX2dt(nspec)
   real(kind=dp_t) :: X_n(nspec), X_np1(nspec), dX(nspec), dfdX(nspec)
   real(kind=dp_t) :: A(nspec, nspec), J(nspec, nspec), b(nspec)
   integer :: info
-  
+
   real(kind=dp_t) :: eps = 1.d-8
 
   real(kind=dp_t) :: rpar(n_rpar_comps)
@@ -51,10 +51,21 @@ subroutine react(Xin, T, rho, tmax, Xout, ierr)
   integer :: max_iter = 20
   integer :: iter, npts, p
   logical :: converged
-  
+
   npts = size(T)
   ierr = 0
 
+  !$acc data copyin(Xin, T, rho, tmax, eps, tol, max_iter, ic12, io16)        &
+  !$acc copyout(Xout)                                                         &
+  !$acc copy(ierr)
+
+  !$acc kernels
+
+  ierr = 0
+  !$acc loop gang vector reduction(max:ierr)                                  &
+  !$acc private(m,n,iter,I,time,dt,converged)                                 &
+  !$acc private(dXdt, X1, X2, dX1dt, dX2dt, X_n, X_np1, dX, A, J, b)          &
+  !$acc private(info, ipiv, rpar, ipar)
   do p = 1, npts
      ! get an estimate of the timestep by looking at the RHS
      ! dt = min{X/(dX/dt)}
@@ -67,7 +78,7 @@ subroutine react(Xin, T, rho, tmax, Xout, ierr)
 
      dt = 1.d33
      dt = 0.01*min(dt, abs(Xin(ih1,p)/dXdt(ih1)))
-  
+
      ! do the integration
      time = ZERO
      do while (time < tmax)
@@ -75,22 +86,22 @@ subroutine react(Xin, T, rho, tmax, Xout, ierr)
 
         ! initial guess
         X_np1(:) = X_n(:)
-     
+
         do iter = 1, max_iter
 
            ! construct a numerical Jacobian via simple differencing
            ! Lapack matrix ordering is described here:
            ! http://www.netlib.org/lapack/lug/node116.html#subsecarrayargs
            ! in general, A(i,j) holds matrix element a_{i,j}
-        
+
            ! J_{m,n} = df_m/dX_n
            do n = 1, nspec
               X1(:) = X_np1(:)
               X1(n) = X1(n) + eps
-              
+
               X2(:) = X_np1(:)
               X2(n) = X2(n) - eps
-              
+
               call rhs(nspec, ZERO, X1, dX1dt, rpar, ipar)
               call rhs(nspec, ZERO, X2, dX2dt, rpar, ipar)
 
@@ -98,7 +109,7 @@ subroutine react(Xin, T, rho, tmax, Xout, ierr)
                  J(m,n) = 0.5d0*(dX2dt(m) - dX1dt(m))/(X2(n) - X1(n))
               enddo
            enddo
-           
+
            ! construct the matrix for the linear system
            ! (I - dt J) dX^{n+1} = rhs
            do n = 1, nspec
@@ -108,21 +119,21 @@ subroutine react(Xin, T, rho, tmax, Xout, ierr)
                  else
                     I = 0.0d0
                  endif
-                 
+
                  A(m,n) = I - dt*J(m,n)
               enddo
            enddo
-           
+
            ! construct the RHS
            call rhs(nspec, ZERO, X_np1, dXdt, rpar, ipar)
            b(:) = X_n(:) - X_np1(:) + dt*dXdt(:)
-           
+
            ! solve the linear system -- we'll just use Lapack.  Note that the
            ! input b is overwritten on output
            call dgesv(nspec, 1, A, nspec, ipiv, b, nspec, info)
-           
+
            dX(:) = b(:)
-           
+
            X_np1(:) = X_np1(:) + dX(:)
 
            ! compare to see if we converged
@@ -130,14 +141,14 @@ subroutine react(Xin, T, rho, tmax, Xout, ierr)
               converged = .true.
               exit
            endif
-           
+
         enddo   ! dX iteration loop
 
         if (.not. converged) then
            ierr = ierr + 1
            exit
         endif
-        
+
         if (time + dt > tmax) then
            dt = tmax - time
         endif
@@ -148,5 +159,6 @@ subroutine react(Xin, T, rho, tmax, Xout, ierr)
 
      Xout(:,p) = X_n(:)
   enddo
+  !$acc end data
 
 end subroutine react
