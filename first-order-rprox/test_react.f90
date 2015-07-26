9bb27f902a346b81edf4719aae4a5c346271fc0c
Michael Zingale <michael.zingale@stonybrook.edu>
2015-07-26 14:06:32 -0400

    reduce tmax by 100x -- it is still a LOT of steps

--- first-order-rprox/test_react.f90
+++ first-order-rprox/test_react.f90
@@ -5,10 +5,10 @@ program main
 
   implicit none
 
-  integer, parameter :: NPTS = 1
+  integer, parameter :: NPTS = 8192
   real(kind=dp_t) :: Xin(nspec, NPTS), Xout(nspec, NPTS)
   real(kind=dp_t) :: rho(NPTS), temp(NPTS)
-  real(kind=dp_t) :: tmax
+  real(kind=dp_t) :: tmax, strt, stp
   integer :: ierr
 
   interface
@@ -28,15 +28,18 @@ program main
   Xin(ih1,:) = 0.7d0
   Xin(ihe4,:) = 0.28d0
   Xin(ic12,:) = 0.02d0
-  
+
   ! burn
   temp(:) = 9.e8
   rho(:) = 2.e6
-  tmax = 0.001
+  tmax = 0.00001
 
+  call cpu_time(strt)
   call react(Xin, temp, rho, tmax, Xout, ierr)
+  call cpu_time(stp)
 
   print *, Xout(:,1)
   print *, ierr
+  print *, 'runtime: ', stp - strt, ' seconds'
 
 end program main
